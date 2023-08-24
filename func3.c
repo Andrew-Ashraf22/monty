f81c28c85f73267c1cd8136d9577abb4bf50092d
Andrew-Ashraf22 <androa45@yahoo.com>
2023-08-24 15:07:50 -0700

    ffix

--- func3.c
+++ func3.c
@@ -1,6 +1,6 @@
 #include "monty.h"
- /**
-  *my_pchar - print char at top
+/**
+  * my_pchar - print char at top
   *@stack: the stack
   *@line_number: num of lines
   */
@@ -15,15 +15,16 @@ void my_pchar(stack_t **stack, unsigned int line_number)
 	}
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
-		fprintf(stderr, "Error: L%d: can't pchar, value out of range\n", line_number);
+		fprintf(stderr, "Error: L%d: can't pchar, value out of range\n",
+		line_number);
 		free_stack(stack);
 		free_info();
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", (*stack)->n);
 }
- /**
-  *my_pstr - print string from top
+/**
+  * my_pstr - print string from top
   *@stack: the stack
   *@line_number: number of lines
   */
@@ -32,7 +33,8 @@ void my_pstr(stack_t **stack, unsigned int line_number)
 	stack_t *current = *stack;
 	(void)line_number;
 
-	while (current != NULL && current->n != 0 && current->n >= 0 && current->n <= 127)
+	while (current != NULL && current->n != 0 &&
+	current->n >= 0 && current->n <= 127)
 	{
 		printf("%c", current->n);
 		current = current->next;
